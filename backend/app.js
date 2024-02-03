3b6e564723ce757875526708c142fd85db45da9f
Sara Gdbg <146811766+SaraGdbg@users.noreply.github.com>
2024-02-03 17:14:00 +0100

    🚚 New routes

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Gör routerna till variabler som används i app.use
+const indexRouter = require("./routes/index");
+const ordersRouter = require("./routes/orders");
+const productsRouter = require("./routes/products");
+const usersRouter = require("./routes/users");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Kopplar ihop routers med app.js
+app.use("/", indexRouter);
+app.use("/api/orders", ordersRouter);
+app.use("/api/products", productsRouter);
+app.use("api/users", usersRouter);
 
 module.exports = app;
